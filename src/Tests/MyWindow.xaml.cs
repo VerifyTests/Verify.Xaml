9181639ff5b38a0addcf287311c0d199958b5660
SimonCropp <simon.cropp@gmail.com>
2020-02-09 11:50:41 +1100

    .

--- src/Tests/MyWindow.xaml.cs
+++ src/Tests/MyWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Tests
 {
@@ -6,6 +7,9 @@ namespace Tests
     {
         public MyWindow()
         {
+            var uri = new Uri("PresentationFramework.Aero;V3.0.0.0;31bf3856ad364e35;component\\themes/aero.normalcolor.xaml", UriKind.Relative);
+
+            Resources.MergedDictionaries.Add((ResourceDictionary) Application.LoadComponent(uri));
             InitializeComponent();
         }
     }
