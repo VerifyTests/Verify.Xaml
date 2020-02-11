1f62463c87606c7c49ad9d1eceef5d2c618952fd
SimonCropp <simon.cropp@gmail.com>
2020-02-11 20:51:48 +1100

    Update MyWindow.xaml.cs

--- src/Tests/MyWindow.xaml.cs
+++ src/Tests/MyWindow.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Windows;
-
-namespace Tests
+﻿namespace Tests
 {
-    public partial class MyWindow : Window
+    public partial class MyWindow
     {
         public MyWindow()
         {
-            var uri = new Uri("PresentationFramework.Aero;V3.0.0.0;31bf3856ad364e35;component\\themes/aero.normalcolor.xaml", UriKind.Relative);
-
-            Resources.MergedDictionaries.Add((ResourceDictionary) Application.LoadComponent(uri));
             InitializeComponent();
         }
     }
