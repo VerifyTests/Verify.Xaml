fae7fa91006a46b26a3cf60c2beab102bde559b4
Simon Cropp <simon.cropp@gmail.com>
2021-04-01 21:17:59 +1100

    fix windows flickering

--- src/Verify.Xaml/WpfUtils.cs
+++ src/Verify.Xaml/WpfUtils.cs
@@ -1,15 +1,17 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Xml;
+using System.Xml.Linq;
 using Verify.Xaml;
 
 static class WpfUtils
 {
-    public static StringBuilder ToXamlString(this FrameworkElement element)
+    public static string ToXamlString(this FrameworkElement element)
     {
         StringBuilder builder = new();
         using StringWriter stringWriter = new(builder);
@@ -18,8 +20,21 @@ static class WpfUtils
             Formatting = Formatting.Indented
         };
         XamlWriter.Save(element, xmlWriter);
-
-        return builder;
+        var document = XDocument.Load(new StringReader(builder.ToString()));
+        var root = document.Root!;
+        foreach (var attribute in root.Attributes().ToList())
+        {
+            var name = attribute.Name.ToString();
+            if (name == "AllowsTransparency" ||
+                name == "ShowInTaskbar" ||
+                name == "WindowStyle"  ||
+                name == "Opacity" ||
+                name == "Visibility" )
+            {
+                attribute.Remove();
+            }
+        }
+        return document.ToString();
     }
 
     public static Stream ScreenCapture(FrameworkElement element)
@@ -37,7 +52,10 @@ static class WpfUtils
         {
             var height = window.Height;
             var width = window.Width;
-            // make sure it is ready for rendering
+            window.ShowInTaskbar = false;
+            window.WindowStyle = WindowStyle.None;
+            window.AllowsTransparency = true;
+            window.Opacity = 0.0;
             window.Show();
             window.Height = height;
             window.Width = width;
