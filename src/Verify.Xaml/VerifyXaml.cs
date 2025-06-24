971bea5d1d04206db70d71f4ac8bf0cf5c436ee2
Simon Cropp <simon.cropp@gmail.com>
2025-06-24 12:11:33 +1000

    4.2.0

--- src/Verify.Xaml/VerifyXaml.cs
+++ src/Verify.Xaml/VerifyXaml.cs
@@ -25,22 +25,21 @@ public static class VerifyXaml
     static ConversionResult ElementToImage(FrameworkElement element, IReadOnlyDictionary<string, object> context)
     {
         var pngStream = WpfUtils.ScreenCapture(element);
-        return new(null,
-            new List<Target>
-            {
+        return new(
+            null,
+            [
                 new("xml", element.ToXamlString()),
                 new("png", pngStream)
-            });
+            ]);
     }
 
     static ConversionResult WindowToImage(Window window, IReadOnlyDictionary<string, object> context)
     {
         var pngStream = WpfUtils.ScreenCapture(window);
         return new(null,
-            new List<Target>
-            {
+            [
                 new("xml", window.ToXamlString()),
                 new("png", pngStream)
-            });
+            ]);
     }
 }
